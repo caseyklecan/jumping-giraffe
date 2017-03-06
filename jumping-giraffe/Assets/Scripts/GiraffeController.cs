19bf2e5f393c67bb6a9ba139bb584adb337fd0e5
Casey Klecan <klecan@usc.edu>
2017-03-06 13:23:32 -0800

    added ground, sky to game; working on making the giraffe jump when he hits the ground

--- jumping-giraffe/Assets/Scripts/GiraffeController.cs
+++ jumping-giraffe/Assets/Scripts/GiraffeController.cs
@@ -19,16 +19,41 @@ public class GiraffeController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown("space")) 
-			{
-			Debug.Log("Space key pressed.");
-			moveDirection.y = moveSpeed; 
-			float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-			transform.rotation = 
-				Quaternion.Slerp( transform.rotation, 
-					Quaternion.Euler( 0, 0, targetAngle ), 
-					turnSpeed * Time.deltaTime );
-			}
+//		if (Input.GetKeyDown("space")) 
+//			{
+//			Debug.Log("Space key pressed.");
+//			moveDirection.y = moveSpeed; 
+//			float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+//			transform.rotation = 
+//				Quaternion.Slerp( transform.rotation, 
+//					Quaternion.Euler( 0, 0, targetAngle ), 
+//					turnSpeed * Time.deltaTime );
+//			}
 		moveDirection.y -= gravity * Time.deltaTime;
+
+		if (onGround) { 
+			if (Input.GetKeyDown ("space"))
+				JumpByForce ();
+			else
+				JumpByImpulse ();
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D c) { 
+		onGround = true;
+	}
+
+	// the jump that occurs when the user does not hit space
+	void JumpByImpulse() { 
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (3.0f * Vector2.up, ForceMode2D.Impulse);
+		onGround = false;
+	}
+
+	// the jump that the user executes
+	void JumpByForce() { 
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
+		rb.AddForce (9.8f * Vector2.up, ForceMode2D.Impulse);
+		onGround = false;
 	}
 }
