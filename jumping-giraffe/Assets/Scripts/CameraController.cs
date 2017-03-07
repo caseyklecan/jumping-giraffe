bb1640b7451cb2b2aae2c81e2d9660b48111dd98
Casey Klecan <klecan@usc.edu>
2017-03-06 18:04:49 -0800

    giraffe moves along, camera follows

---
+++ jumping-giraffe/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private Transform giraffe;
+
+	// Use this for initialization
+	void Start () {
+		giraffe = GameObject.Find ("Giraffe").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPos = giraffe.position;
+		newPos.y = transform.position.y;
+		newPos.z = transform.position.z;
+		transform.position = newPos;
+	}
+}
--- jumping-giraffe/Assets/Scripts/GiraffeController.cs
+++ jumping-giraffe/Assets/Scripts/GiraffeController.cs
@@ -8,6 +8,12 @@ public class GiraffeController : MonoBehaviour {
 	public float turnSpeed;
 	public float jumpHeight;
 	public float gravity;
+
+	public float transformDist = 3f;
+	public float bounceForce = 4f;
+	public float jumpForce = 6f;
+
+
 	private bool onGround =  false;
 	private Vector3 moveDirection = Vector3.zero;
 
@@ -19,24 +25,33 @@ public class GiraffeController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-//		if (Input.GetKeyDown("space")) 
-//			{
-//			Debug.Log("Space key pressed.");
-//			moveDirection.y = moveSpeed; 
-//			float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
-//			transform.rotation = 
-//				Quaternion.Slerp( transform.rotation, 
-//					Quaternion.Euler( 0, 0, targetAngle ), 
-//					turnSpeed * Time.deltaTime );
-//			}
+		// how the giraffe moves
+		Vector3 newPos = new Vector3 (transform.position.x + transformDist, transform.position.y, transform.position.z);
+		transform.position = Vector3.Lerp (transform.position, newPos, Time.deltaTime);
+
+
+		if (Input.GetKeyDown("space")) 
+			{
+			Debug.Log("Space key pressed.");
+			moveDirection.y = moveSpeed; 
+			float targetAngle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+			transform.rotation = 
+				Quaternion.Slerp( transform.rotation, 
+					Quaternion.Euler( 0, 0, targetAngle ), 
+					turnSpeed * Time.deltaTime );
+			}
 		moveDirection.y -= gravity * Time.deltaTime;
 
+
+
 		if (onGround) { 
-			if (Input.GetKeyDown ("space"))
-				JumpByForce ();
-			else
+//			if (Input.GetKeyDown ("space"))
+//				JumpByForce ();
+//			else
 				JumpByImpulse ();
 		}
+
+
 	}
 
 	void OnCollisionEnter2D(Collision2D c) { 
@@ -46,14 +61,14 @@ public class GiraffeController : MonoBehaviour {
 	// the jump that occurs when the user does not hit space
 	void JumpByImpulse() { 
 		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
-		rb.AddForce (3.0f * Vector2.up, ForceMode2D.Impulse);
+		rb.AddForce (bounceForce * Vector2.up, ForceMode2D.Impulse);
 		onGround = false;
 	}
 
 	// the jump that the user executes
 	void JumpByForce() { 
 		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
-		rb.AddForce (9.8f * Vector2.up, ForceMode2D.Impulse);
+		rb.AddForce (jumpForce * Vector2.up, ForceMode2D.Impulse);
 		onGround = false;
 	}
 }
