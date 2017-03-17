48c31ffc53d5fbc7b9a721108e5444c16706005a
LAPTOP-565ROL5C\alexa <alexanwm@usc.edu>
2017-03-16 23:06:22 -0700

    Working on making speed and invincibility power ups appear automatically at random intervals

--- jumping-giraffe/Assets/Scripts/SpeedController.cs
+++ jumping-giraffe/Assets/Scripts/SpeedController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpeedController : MonoBehaviour {
+    public float minCreationTime = 15f;
+    public float maxCreationTime = 200f;
+    public GameObject speedPrefab;
 
 	// Use this for initialization
 	void Start () {
-		
+        Invoke("CreateSpeedPowerUp", minCreationTime);
 	}
 	
 	// Update is called once per frame
@@ -20,5 +23,17 @@ public class SpeedController : MonoBehaviour {
         {
             Destroy(gameObject);
         }
+    }
+
+    void CreateSpeedPowerUp()
+    {
+        Camera camera = Camera.main;
+        Vector3 cameraPos = camera.transform.position;
+        float xMax = camera.aspect * camera.orthographicSize;
+        float xRange = camera.aspect * camera.orthographicSize * 1.75f;
+        float yMax = camera.orthographicSize - 0.5f;
+        Vector3 speedPos = new Vector3(cameraPos.x + Random.Range(xMax - xRange, xMax), Random.Range (-yMax, yMax), speedPrefab.transform.position.z);
+        Instantiate(speedPrefab, speedPos, Quaternion.identity);
+        Invoke("CreateSpeedPowerUp", Random.Range(minCreationTime, maxCreationTime));
     }
 }
