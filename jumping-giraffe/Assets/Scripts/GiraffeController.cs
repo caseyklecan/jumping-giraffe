072a44b4ab7b0afca626f5afc4941fc6b38bfc6a
LAPTOP-565ROL5C\alexa <alexanwm@usc.edu>
2017-03-18 10:35:40 -0700

    Fixed giraffe animation

--- jumping-giraffe/Assets/Scripts/GiraffeController.cs
+++ jumping-giraffe/Assets/Scripts/GiraffeController.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class GiraffeController : MonoBehaviour {
 
@@ -49,6 +48,14 @@ public class GiraffeController : MonoBehaviour {
 
 
 		}
+
+        if (!onGround)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                GetComponent<Animator>().Play("GiraffeFlip");
+            }
+        }
 
 
 	}
