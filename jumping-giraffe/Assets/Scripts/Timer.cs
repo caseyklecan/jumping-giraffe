8c23fb5300b91a2134aabaaa1f5156bd3424210e
Alexandria Mar <alexanwm@usc.edu>
2017-04-12 14:23:26 -0700

    Added a timer

---
+++ jumping-giraffe/Assets/Scripts/Timer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Timer : MonoBehaviour {
+
+	public float time;
+	public Text timerGT;
+	GameObject timerGO;
+
+	// Use this for initialization
+	void Start () {
+		timerGO = GameObject.Find ("Timer");
+		timerGT = timerGO.GetComponent<Text> ();
+		timerGT.text = "30.0";
+		time = 30.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+		timerGT.text = time.ToString();
+		if (time < 0.0f) {
+			//timerGO.GetComponent<GiraffeController> ().Win ();
+		}
+	}
+}
